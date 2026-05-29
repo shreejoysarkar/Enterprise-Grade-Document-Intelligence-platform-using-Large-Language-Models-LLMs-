aa37d2eb4b4e4b37fd1a141d1f2feab3704c4bdd
shreejoy <sarkarshreejoy30@gmail.com>
2026-05-29 21:15:07 +0530

    feat: include utils package in setup configuration

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="doc_intel",
     version="0.1",
-    packages=find_packages(include=['core', 'doc_intel']),
+    packages=find_packages(include=['core', 'doc_intel', 'utils']),
 )
